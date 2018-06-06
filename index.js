e072311d2cbd05710c1f200270ab043136f28d9e
Tim Turner <timmyturner@me.com>
2018-06-06 13:32:04 -0700

    adds mongo uri pointing to mLab

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
